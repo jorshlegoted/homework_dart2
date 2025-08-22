e5c90984299baee440168798629a4e2d39bc214a
jorshlegoted <no>
2025-08-22 21:35:58 +0300

    Третье дз вася

--- lib/main.dart
+++ lib/main.dart
@@ -1,144 +1,73 @@
 import 'package:flutter/material.dart';
+import 'profession.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
+  
 }
+final vasya = Human(name: 'Вася', age: 20, profession: ProfessionType.student, gender: GenderType.male);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-
-  }
-
-  void _dicrementCounter() {
-    setState(() {
-      if (_counter == 0) { // ниже 0 теперь счётчик не уменьшается
-        return;
-      }
-      _counter--;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(200, 238, 255, 3),
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 1, 1, 255),
+          title: Text(vasya.name, style: TextStyle(color: Colors.white),),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            
+            // Container(height: 400, width: 400, color: Color.fromARGB(186, 211, 230, 108), child: 
+            Container(height: 400, width: 500, color: Colors.amber, alignment: Alignment.centerRight,
+            child: Text('Описание профиля: \nПол: ${vasya.gender.genderType}\nВозраст: ${vasya.age}\nДолжность: ${vasya.profession.professionType}'),),
+            
+            
           ],
         ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          FloatingActionButton(
-            onPressed: _dicrementCounter,
-            tooltip: 'Dicrement',
-            child: const Icon(Icons.remove),
-          ),
-          const SizedBox(width: 16),
-          FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          ),
-        ],
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        
+        
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Color.fromARGB(255, 47, 189, 111),
+        
+          
+          child: Icon(Icons.favorite),
+
+        
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      )
     );
+          
   }
 }
+
+class Human {
+  const Human({required this.name, required this.age, required this.profession, required this.gender});
+
+  final String name;
+  final int age;
+  final GenderType gender;
+  final ProfessionType profession;
+
+
+  @override
+  String toString() => 'Имя $name\nВозраст: $age\nДолжность: ${profession.professionType}\nПол: ${gender.genderType}';
+}
+
+enum GenderType {
+  male('Мужской'),
+  woman('Женский');
+
+  final String genderType;
+  const GenderType(this.genderType);
+}
+
+
+
---
+++ lib/profession.dart
@@ -0,0 +1,8 @@
+enum ProfessionType {
+  student('Студент'),
+  worker('Сотрудник'),
+  kid('Ребёнок');
+
+  final String professionType;
+  const ProfessionType(this.professionType);
+}
